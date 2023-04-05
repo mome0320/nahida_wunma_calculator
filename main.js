8e8a1c3ed5b80399798636fd787deb0213485c61
Hotococoa6078 <71052778+Hotococoa6078@users.noreply.github.com>
2023-04-05 20:21:29 +0900

    asdf

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 function LevelChanged(){
-    const CL = document.getElementById('Clevel').selectedIndex
+    var CL = document.getElementById('Clevel').selectedIndex
     document.getElementById("LevelImage").src = `./image/levels/level${CL}.PNG`
+}
+function WLevelChanged(){
+    var WL = document.getElementById('WLevel').selectedIndex
+    document.getElementById("WeaponLevelImage").src = `./image/levels/level${WL}.PNG`
+}
+function jaeryonChanged(){
+    var jaeryon = document.getElementsByClassName('jaeryonSlider')
+    var output = document.getElementById('jaeryonValue')
+    output.innerHTML = jaeryon.value
+    console.log(jaeryon)
 }
